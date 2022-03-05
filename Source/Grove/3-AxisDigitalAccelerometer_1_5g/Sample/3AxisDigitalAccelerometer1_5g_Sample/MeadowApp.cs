c780d6468beaef9d1f83985bc9ef5929b16b4300
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-04 20:09:39 -0800

    Add projects for acceleromters and compass

---
+++ Source/Grove/3-AxisDigitalAccelerometer_1_5g/Sample/3AxisDigitalAccelerometer1_5g_Sample/MeadowApp.cs
@@ -0,0 +1,17 @@
+﻿using Meadow;
+using Meadow.Devices;
+using System;
+
+namespace Grove.FourDigitDisplay_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing ...");
+
+         
+        }
+    }
+}
---
+++ Source/Grove/3-AxisDigitalCompass/Driver/ThreeAxisDigitalCompass.cs
@@ -0,0 +1,13 @@
+﻿using Meadow.Devices;
+using Meadow.Foundation.Displays;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Displays
+{
+    public class FourDigitDisplay : Tm1637
+    {
+        public FourDigitDisplay(IMeadowDevice device, IPin pinClock, IPin pinData) : base(device, pinClock, pinData)
+        {
+        }
+    }
+}
