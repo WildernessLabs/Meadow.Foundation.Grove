d9fe896af586fa95f9b9e13bf3dd388c8ae7e2d9
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-04 19:47:18 -0800

    Add LED Socket driver

---
+++ Source/Grove/LEDSocket/Driver/LEDSocket.cs
@@ -0,0 +1,16 @@
+﻿using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Leds
+{
+    public class LEDSocket : Led
+    {
+        public LEDSocket(IDigitalOutputController device, IPin pin) : base(device, pin)
+        {
+        }
+
+        public LEDSocket(IDigitalOutputPort port) : base(port)
+        {
+        }
+    }
+}
---
+++ Source/Grove/LEDSocket/Sample/LEDSocket_Sample/MeadowApp.cs
@@ -0,0 +1,20 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Leds;
+using System;
+
+namespace Grove.LEDSocket_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            LEDSocket led = new LEDSocket(Device, Device.Pins.D13);
+
+            led.StartBlink();
+        }
+    }
+}
