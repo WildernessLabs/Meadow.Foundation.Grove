ce5e55ddf31ecfd58262564a7efe7fd17fbbe2e1
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-05 19:05:53 -0800

    Add Grove Buzzer driver

---
+++ Source/Grove/Buzzer/Driver/Buzzer.cs
@@ -0,0 +1,18 @@
+﻿using Meadow.Foundation.Audio;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Audio
+{
+    public class Buzzer : PiezoSpeaker
+    {
+        public Buzzer(IPwmOutputController device, IPin pin, float frequency = 100, float dutyCycle = 0)
+            : base(device, pin, frequency, dutyCycle)
+        {
+        }
+
+        public Buzzer(IPwmPort port)
+            : base(port)
+        {
+        }
+    }
+}
---
+++ Source/Grove/Buzzer/Sample/Buzzer_Sample/MeadowApp.cs
@@ -0,0 +1,36 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Audio;
+using System;
+using System.Threading.Tasks;
+
+namespace Grove.Buzzer_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        readonly Buzzer buzzer;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            buzzer = new Buzzer(Device.CreatePwmPort(Device.Pins.D13));
+
+            _ = PlayTriad();
+        }
+
+        async Task PlayTriad()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Playing A major triad starting at A4");
+                await buzzer.PlayTone(440, 500); //A
+                await buzzer.PlayTone(554.37f, 500); //C#
+                await buzzer.PlayTone(659.25f, 500); //E
+                
+                await Task.Delay(2500);
+            }
+        }
+    }
+}
