15116ff32bf2166689e3d834a4a4f9435ec4e23f
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-09 14:03:58 -0800

    Add Grove Switch(P)

---
+++ Source/Grove/Switch(P)/Driver/Switch(P).cs
@@ -0,0 +1,13 @@
+﻿using Meadow.Foundation.Sensors.Switches;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Sensors.Switches
+{
+    public class SwitchP : SpdtSwitch
+    {
+        public SwitchP(IDigitalInputController device, IPin pin) 
+            : base(device, pin, InterruptMode.EdgeBoth, ResistorMode.InternalPullDown, 20, 0)
+        {
+        }
+    }
+}
