7ade968c739e890dbd13d8875998071a1325d7bf
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-08 20:59:11 -0800

    Add RTC driver and sample

---
+++ Source/Grove/RTC/Driver/RTC.cs
@@ -0,0 +1,12 @@
+﻿using Meadow.Foundation.RTCs;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.RTCs
+{
+    public class RTC : Ds1307
+    {
+        public RTC(II2cBus bus) : base(bus)
+        {
+        }
+    }
+}
---
+++ Source/Grove/RTC/Sample/RTC_Sample/MeadowApp.cs
@@ -0,0 +1,55 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.RTCs;
+using System;
+
+namespace Grove.RTC_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing...");
+
+            var rtc = new RTC(Device.CreateI2cBus());
+
+            Console.WriteLine("rtc created");
+
+            var running = rtc.IsRunning;
+
+            Console.WriteLine($"{(running ? "is running" : "is not running")}");
+
+            if (!running)
+            {
+                Console.WriteLine(" Starting RTC...");
+                rtc.IsRunning = true;
+            }
+
+            var dateTime = rtc.GetTime();
+            Console.WriteLine($" RTC current time is: {dateTime:MM/dd/yy HH:mm:ss}");
+
+            Console.WriteLine($" Setting RTC to : {dateTime:MM/dd/yy HH:mm:ss}");
+            dateTime = new DateTime(2030, 2, 15);
+            rtc.SetTime(dateTime);
+
+            dateTime = rtc.GetTime();
+            Console.WriteLine($" RTC current time is: {dateTime:MM/dd/yy HH:mm:ss}");
+
+            Random rand = new Random();
+
+            var data = new byte[56];
+
+            for (int i = 0; i < 56; i++)
+            {
+                data[i] = (byte)rand.Next(256);
+            }
+
+            Console.WriteLine($" Writing to RTC RAM   : {BitConverter.ToString(data)}");
+            rtc.WriteRAM(0, data);
+            Console.Write($" Reading from RTC RAM : ");
+            data = rtc.ReadRAM(0, 56);
+            Console.WriteLine(BitConverter.ToString(data));
+        }
+    }
+}
