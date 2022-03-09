95203ae4c36c97c8335dcd6472d7d3ebc5ac0f7f
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-08 21:24:43 -0800

    Add ThumbJoystick

---
+++ Source/Grove/ThumbJoystick/Driver/ThumbJoystick.cs
@@ -0,0 +1,21 @@
+﻿using Meadow.Foundation.Sensors.Hid;
+using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Hid
+{
+    public class ThumbJoystick : AnalogJoystick
+    {
+        public ThumbJoystick(IAnalogInputPort horizontalInputPort, IAnalogInputPort verticalInputPort, JoystickCalibration calibration = null, bool isInverted = false) : base(horizontalInputPort, verticalInputPort, calibration, isInverted)
+        {
+        }
+
+        public ThumbJoystick(IAnalogInputController device, IPin horizontalPin, IPin verticalPin, JoystickCalibration calibration = null, bool isInverted = false) : base(device, horizontalPin, verticalPin, calibration, isInverted)
+        {
+        }
+
+        public ThumbJoystick(IAnalogInputController device, IPin horizontalPin, IPin verticalPin, JoystickCalibration calibration, bool isInverted, int sampleCount, TimeSpan sampleInterval) : base(device, horizontalPin, verticalPin, calibration, isInverted, sampleCount, sampleInterval)
+        {
+        }
+    }
+}
---
+++ Source/Grove/ThumbJoystick/Sample/ThumbJoystick_Sample/MeadowApp.cs
@@ -0,0 +1,48 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Hid;
+using Meadow.Peripherals.Sensors.Hid;
+using Meadow.Units;
+using System;
+
+namespace Grove.ThumbJoystick_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        ThumbJoystick joystick;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            // assume that the stick is in the center when it starts up
+            _ = joystick.SetCenterPosition(); //fire and forget
+
+            joystick.Updated += JoystickUpdated;
+
+            joystick.StartUpdating(TimeSpan.FromMilliseconds(20));
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initializing hardware...");
+
+            //==== Joystick
+            // these are pretty fast updates (40ms in total), if you need more time to process, you can
+            // increase the sample interval duration and/or standby duration.
+            joystick = new ThumbJoystick(
+                Device.CreateAnalogInputPort(Device.Pins.A01, 1, TimeSpan.FromMilliseconds(10), new Voltage(3.3)),
+                Device.CreateAnalogInputPort(Device.Pins.A02, 1, TimeSpan.FromMilliseconds(10), new Voltage(3.3)),
+                null, false);
+
+            Console.WriteLine("Hardware initialization complete.");
+        }
+
+        void JoystickUpdated(object sender, IChangeResult<JoystickPosition> e)
+        {
+            Console.WriteLine($"Horizontal: {e.New.Horizontal:n2}, Vertical: {e.New.Vertical:n2}");
+            Console.WriteLine($"Digital position: {joystick.DigitalPosition}");
+        }
+    }
+}
