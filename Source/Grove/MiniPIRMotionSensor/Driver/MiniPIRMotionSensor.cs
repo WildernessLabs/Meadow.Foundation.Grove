82893f2dcdafeb2d8fef10cdb05e5a9bf4d2f9af
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-08 21:10:51 -0800

    Add MiniPIRMotionSensor

---
+++ Source/Grove/MiniPIRMotionSensor/Driver/MiniPIRMotionSensor.cs
@@ -0,0 +1,16 @@
+﻿using Meadow.Foundation.Sensors.Motion;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Sensors.Motion
+{
+    public class MiniPIRMotionSensor : ParallaxPir
+    {
+        public MiniPIRMotionSensor(IDigitalInputController device, IPin pin, InterruptMode interruptMode, ResistorMode resistorMode, int debounceDuration = 20, int glitchFilterCycleCount = 0) : base(device, pin, interruptMode, resistorMode, debounceDuration, glitchFilterCycleCount)
+        {
+        }
+    
+        public MiniPIRMotionSensor(IDigitalInputPort digitalInputPort) : base(digitalInputPort)
+        {
+        }
+    }
+}
---
+++ Source/Grove/MiniPIRMotionSensor/Sample/MiniPIRMotionSensor_Sample/MeadowApp.cs
@@ -0,0 +1,19 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Motion;
+using Meadow.Hardware;
+using System;
+
+namespace Grove.MiniPIRMotionSensor_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            var miniPIRMotionSensor = new MiniPIRMotionSensor(Device.CreateDigitalInputPort(Device.Pins.D13, InterruptMode.EdgeBoth, ResistorMode.Disabled));
+            miniPIRMotionSensor.OnMotionStart += (sender) => Console.WriteLine($"Motion start  {DateTime.Now}");
+            miniPIRMotionSensor.OnMotionEnd += (sender) => Console.WriteLine($"Motion end  {DateTime.Now}");
+        }
+    }
+}
