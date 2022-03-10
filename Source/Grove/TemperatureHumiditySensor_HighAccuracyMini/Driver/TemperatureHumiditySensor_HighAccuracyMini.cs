ad48c3393f8ccce2c161dd3afaaab7aedabccab4
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-09 22:26:25 -0800

    Add Temp Humidity Sensor HAMini

---
+++ Source/Grove/TemperatureHumiditySensor_HighAccuracyMini/Driver/TemperatureHumiditySensor_HighAccuracyMini.cs
@@ -0,0 +1,12 @@
+﻿using Meadow.Foundation.Sensors.Atmospheric;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Sensors.Atmospheric
+{
+    public class TemperatureHumiditySensor_HighAccuracyMini : Th02
+    {
+        public TemperatureHumiditySensor_HighAccuracyMini(II2cBus i2cBus) : base(i2cBus, 64)
+        {
+        }
+    }
+}
---
+++ Source/Grove/TemperatureHumiditySensor_HighAccuracyMini/Sample/TemperatureHumiditySensor_HighAccuracyMini_Sample/MeadowApp.cs
@@ -0,0 +1,44 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Atmospheric;
+using System;
+
+namespace Grove.TemperatureHumiditySensor_HighAccuracyMini_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var sensor = new TemperatureHumiditySensor_HighAccuracyMini(Device.CreateI2cBus());
+
+            var consumer = TemperatureHumiditySensor_HighAccuracyMini.CreateObserver(
+                handler: result =>
+                {
+                    Console.WriteLine($"Observer: Temp changed by threshold; new temp: {result.New.Temperature?.Celsius:N2}C, old: {result.Old?.Temperature?.Celsius:N2}C");
+                },
+                filter: result =>
+                {
+                    //c# 8 pattern match syntax. checks for !null and assigns var.
+                    if (result.Old is { } old)
+                    {
+                        return (
+                        (result.New.Temperature.Value - old.Temperature.Value).Abs().Celsius > 0.5);
+                    }
+                    return false;
+                }
+            );
+            sensor.Subscribe(consumer);
+
+            sensor.Updated += (sender, result) =>
+            {
+                Console.WriteLine($"  Temperature: {result.New.Temperature?.Celsius:N2}C");
+                Console.WriteLine($"  Relative Humidity: {result.New.Humidity?.Percent:N2}%");
+            };
+
+            sensor.StartUpdating(TimeSpan.FromSeconds(1));
+        }
+    }
+}
