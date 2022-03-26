cebec0aeffdb20df74b1f74cd32c480bd5087bbc
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 19:51:12 -0700

    [TemperatureSensor] Add driver and sample

---
+++ Source/Grove/TemperatureSensor/Driver/TemperatureSensor.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Foundation.Sensors.Base;
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Temperature
+{
+    /// <summary>
+    /// Represents a Temperature Sensor
+    /// </summary>
+    public class TemperatureSensor : AnalogObservableBase
+    {
+        /// <summary>
+        /// Creates a new TemperatureSensor driver
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public TemperatureSensor(IAnalogInputController device, IPin pin, int sampleCount = 5, TimeSpan? sampleInterval = null, Voltage? voltage = null)
+            : this(device.CreateAnalogInputPort(pin, sampleCount, sampleInterval ?? TimeSpan.FromMilliseconds(40), voltage ?? new Voltage(3.3)))
+        { }
+
+        /// <summary>
+        /// Creates a new TemperatureSensor driver
+        /// </summary>
+        /// <param name="port"></param>
+        public TemperatureSensor(IAnalogInputPort port) : base(port)
+        { }
+    }
+}
---
+++ Source/Grove/TemperatureSensor/Sample/TemperatureSensor_Sample/MeadowApp.cs
@@ -0,0 +1,58 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Temperature;
+using System;
+using System.Threading.Tasks;
+
+namespace TemperatureSensor_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        //<!—SNIP—>
+
+        TemperatureSensor sensor;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing...");
+
+            // configure our sensor
+            sensor = new TemperatureSensor(Device, Device.Pins.A01);
+
+            // Example that uses an IObservable subscription to only be notified when the voltage changes by at least 500mV
+            var consumer = TemperatureSensor.CreateObserver(
+                handler: result => Console.WriteLine($"Observer filter satisfied: {result.New.Millivolts:N2}mV, old: {result.Old?.Millivolts:N2}mV"),
+                // only notify if the change is greater than 0.5V
+                filter: result => 
+                {
+                    if (result.Old is { } old)
+                    { //c# 8 pattern match syntax. checks for !null and assigns var.
+                        return (result.New - old).Abs().Millivolts > 500;
+                    }
+                    return false;
+                });
+
+            sensor.Subscribe(consumer);
+
+            // classical .NET events can also be used:
+            sensor.Updated += (sender, result) => 
+            {
+                Console.WriteLine($"Voltage Changed, new: {result.New.Millivolts:N2}mV, old: {result.Old?.Millivolts:N2}mV");
+            };
+
+            //==== One-off reading use case/pattern
+            Read().Wait();
+
+            sensor.StartUpdating(TimeSpan.FromMilliseconds(1000));
+        }
+
+        protected async Task Read()
+        {
+            var result = await sensor.Read();
+            Console.WriteLine($"Initial read: {result.Millivolts:N2}mV");
+        }
+
+        //<!—SNOP—>
+    }
+}
