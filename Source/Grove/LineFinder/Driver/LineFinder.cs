e03821768f214ddb8146eb5640237747dc8cb6ec
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 21:53:39 -0700

    [LineFinder] Add driver and sample

---
+++ Source/Grove/LineFinder/Driver/LineFinder.cs
@@ -0,0 +1,52 @@
+﻿using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Light
+{
+    /// <summary>
+    /// Represents a line finder sensor
+    /// </summary>
+    public class LineFinder 
+    {
+        IDigitalInputPort inputPort;
+
+        /// <summary>
+        /// Returns Line Color
+        /// </summary>
+        public enum LineColor { White, Black }
+
+        /// <summary>
+        /// Last state of the Line Finder
+        /// </summary>
+        public LineColor Color => inputPort.State ? LineColor.Black : LineColor.White;
+
+        /// <summary>
+        /// Event when line color changed
+        /// </summary>
+        public event EventHandler<LineColor> ColorChanged = delegate { };
+
+        /// <summary>
+        /// Creates a new LineFinder driver
+        /// </summary>
+        /// <param name="signalPort"></param>
+        public LineFinder(IDigitalInputPort signalPort)
+        {
+            inputPort = signalPort;
+            signalPort.Changed += SignalPortChanged;
+        }
+
+        /// <summary>
+        /// Creates a new LineFinder driver
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="inputPin"></param>
+        public LineFinder(IDigitalInputController device, IPin inputPin) 
+            : this(device.CreateDigitalInputPort(inputPin, InterruptMode.EdgeBoth, ResistorMode.InternalPullUp, 0, 25))
+        { }
+
+        private void SignalPortChanged(object sender, DigitalPortResult e)
+        {
+            ColorChanged?.Invoke(this, e.New.State? LineColor.Black : LineColor.White);
+        }
+    }
+}
---
+++ Source/Grove/LineFinder/Sample/LineFinder_Sample/MeadowApp.cs
@@ -0,0 +1,20 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Light;
+using System;
+
+namespace Grove.LineFinder_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var lineFinder = new LineFinder(Device, Device.Pins.D13);
+
+            lineFinder.ColorChanged += (s, e) => Console.WriteLine($"line color: {e}");
+        }
+    }
+}
