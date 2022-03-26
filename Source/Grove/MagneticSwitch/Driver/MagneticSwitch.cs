e22c9aa918ee341253e882d255d0a34538824121
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-25 19:10:02 -0700

    Add magnetic switch

---
+++ Source/Grove/MagneticSwitch/Driver/MagneticSwitch.cs
@@ -0,0 +1,32 @@
+﻿using Meadow.Hardware;
+using Meadow.Peripherals.Switches;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Switches
+{
+    public class MagneticSwitch : ISwitch
+    {
+        public bool IsOn => port.State;
+
+        public event EventHandler Changed = delegate { };
+
+        IDigitalInputPort port;
+
+        public MagneticSwitch(IDigitalInputPort signalPort)
+        {
+            port = signalPort;
+            
+            signalPort.Changed += SignalPort_Changed;
+        }
+
+        private void SignalPort_Changed(object sender, DigitalPortResult e)
+        {
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        public MagneticSwitch(IDigitalInputController device, IPin inputPin) 
+            : this(device.CreateDigitalInputPort(inputPin, InterruptMode.EdgeBoth, ResistorMode.InternalPullUp, 0, 25))
+        {
+        }
+    }
+}
---
+++ Source/Grove/MagneticSwitch/Sample/MagneticSwitch_Sample/MeadowApp.cs
@@ -0,0 +1,20 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Sensors.Switches;
+using System;
+
+namespace Grove.FlameSensor_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var magneticSwitch = new MagneticSwitch(Device, Device.Pins.D13);
+
+            magneticSwitch.Changed += (s, e) => Console.WriteLine($"Switched - open {magneticSwitch.IsOn}");
+        }
+    }
+}
