422ef34e42229ac3df44b2af492918f71100e551
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 20:07:01 -0700

    [SoundSensor] Add driver and sample

---
+++ Source/Grove/SoundSensor/Driver/SoundSensor.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Foundation.Sensors.Base;
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Sound
+{
+    /// <summary>
+    /// Represents a Sound Sensor
+    /// </summary>
+    public class SoundSensor : AnalogObservableBase
+    {
+        /// <summary>
+        /// Creates a new SoundSensor driver
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public SoundSensor(IAnalogInputController device, IPin pin, int sampleCount = 5, TimeSpan? sampleInterval = null, Voltage? voltage = null)
+            : this(device.CreateAnalogInputPort(pin, sampleCount, sampleInterval ?? TimeSpan.FromMilliseconds(40), voltage ?? new Voltage(3.3)))
+        { }
+
+        /// <summary>
+        /// Creates a new SoundSensor driver
+        /// </summary>
+        /// <param name="port"></param>
+        public SoundSensor(IAnalogInputPort port) : base(port)
+        { }
+    }
+}
