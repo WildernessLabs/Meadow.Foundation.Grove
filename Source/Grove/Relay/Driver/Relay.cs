a67e65401d8113f094cf086e48afde9a624da9f8
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 20:52:16 -0700

    [Relay] Add driver and sample

---
+++ Source/Grove/Relay/Driver/Relay.cs
@@ -0,0 +1,25 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Relays
+{
+    /// <summary>
+    /// Represents a Relay
+    /// </summary>
+    public class Relay : Foundation.Relays.Relay
+    {
+        /// <summary>
+        /// Creates a new Relay driver
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="pin"></param>
+        public Relay(IDigitalOutputController device, IPin pin) : base(device, pin)
+        { }
+
+        /// <summary>
+        /// Creates a new Relay driver
+        /// </summary>
+        /// <param name="port"></param>
+        public Relay(IDigitalOutputPort port) : base(port)
+        { }
+    }
+}
---
+++ Source/Grove/Relay/Sample/Relay_Sample/MeadowApp.cs
@@ -0,0 +1,40 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Relays;
+using System;
+using System.Threading;
+
+namespace Relay_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        Relay relay;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            relay = new Relay(Device, Device.Pins.D13);
+
+            TestRelay();
+        }
+
+        void TestRelay()
+        {
+            Console.WriteLine("TestRelay...");
+
+            var state = false;
+
+            while (true)
+            {
+                state = !state;
+
+                Console.WriteLine($"- State: {state}");
+                relay.IsOn = state;
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
