93f4404b297b812415df3bd8fc147a853e66363e
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 19:17:57 -0700

    [RotaryAngleSensor] Add driver and sample

---
+++ Source/Grove/RotaryAngleSensor/Driver/RotaryAngleSensor.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Foundation.Sensors.Base;
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+
+namespace Meadow.Foundation.Sensors.Rotary
+{
+    /// <summary>
+    /// Represents a rotary angle sensor
+    /// </summary>
+    public class RotaryAngleSensor : AnalogObservableBase
+    {
+        /// <summary>
+        /// Creates a new RotaryAngleSensor driver
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public RotaryAngleSensor(IAnalogInputController device, IPin pin, int sampleCount = 5, TimeSpan? sampleInterval = null, Voltage? voltage = null)
+            : this(device.CreateAnalogInputPort(pin, sampleCount, sampleInterval ?? TimeSpan.FromMilliseconds(40), voltage ?? new Voltage(3.3)))
+        { }
+
+        /// <summary>
+        /// Creates a new RotaryAngleSensor driver
+        /// </summary>
+        /// <param name="port"></param>
+        public RotaryAngleSensor(IAnalogInputPort port) : base(port)
+        { }
+    }
+}
