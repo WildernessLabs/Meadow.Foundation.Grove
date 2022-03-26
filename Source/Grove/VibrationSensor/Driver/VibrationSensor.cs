f85e7ec121122a40a1b292ca69c5ff284e4fb65b
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-25 17:44:58 -0700

    Add vibration sensor

---
+++ Source/Grove/VibrationSensor/Driver/VibrationSensor.cs
@@ -0,0 +1,26 @@
+﻿using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Grove.Sensors.Motion
+{
+    public class VibrationSensor 
+    {
+        public event EventHandler VibrationDetected = delegate { };
+
+        public VibrationSensor(IDigitalInputPort signalPort)
+        {
+            signalPort.Changed += SignalPort_Changed;
+        }
+
+        private void SignalPort_Changed(object sender, DigitalPortResult e)
+        {
+            VibrationDetected?.Invoke(this, EventArgs.Empty);
+        }
+
+        public VibrationSensor(IDigitalInputController device, IPin inputPin) 
+            : this(device.CreateDigitalInputPort(inputPin, InterruptMode.EdgeRising, ResistorMode.InternalPullUp, 0, 25))
+        {
+
+        }
+    }
+}
