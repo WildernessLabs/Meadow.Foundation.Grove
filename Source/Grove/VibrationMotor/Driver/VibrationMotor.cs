34b41672e59f0178c371d312e638a446523de10b
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-25 19:36:50 -0700

    Add vibration motor driver

---
+++ Source/Grove/VibrationMotor/Driver/VibrationMotor.cs
@@ -0,0 +1,32 @@
+﻿using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.HID
+{
+    public class VibrationMotor
+    {
+        public bool IsOn => port.State;
+
+        readonly IDigitalOutputPort port;
+
+        public VibrationMotor(IDigitalOutputController device, IPin pin) 
+            : this(device.CreateDigitalOutputPort(pin, false))
+        {
+        }
+
+        public VibrationMotor(IDigitalOutputPort port)
+        {
+            this.port = port;
+        }
+
+        public void StartVibration()
+        {
+            port.State = true;
+        }
+
+        public void StopVibration()
+        {
+            port.State = false;
+        }
+    }
+}
---
+++ Source/Grove/VibrationMotor/Sample/VibrationMotor_Sample/MeadowApp.cs
@@ -0,0 +1,32 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.HID;
+using System;
+using System.Threading;
+
+namespace Grove.VibrationMotor_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var vibrationMotor = new VibrationMotor(Device, Device.Pins.D13);
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Motor on");
+                vibrationMotor.StartVibration();
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Motor off");
+                vibrationMotor.StopVibration();
+                Thread.Sleep(1000);
+
+
+            }
+        }
+    }
+}
