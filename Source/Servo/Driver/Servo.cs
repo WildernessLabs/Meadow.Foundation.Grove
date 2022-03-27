e0bf05c289187853bfbd8eeee357c7002593f86b
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-26 23:22:16 -0700

    Add Servo

---
+++ Source/Servo/Driver/Servo.cs
@@ -0,0 +1,19 @@
+﻿using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Foundation.Servos;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Servos
+{
+    public class Servo : Meadow.Foundation.Servos.Servo
+    {
+        public Servo(IPwmPort pwm)
+            : base(pwm, NamedServoConfigs.SG90)
+        {
+        }
+
+        public Servo(IPwmOutputController device, IPin pwm)
+            : base(device, pwm, NamedServoConfigs.SG90)
+        {
+        }
+    }
+}
---
+++ Source/Servo/Sample/Servo_Sample/MeadowApp.cs
@@ -0,0 +1,41 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Servos;
+using Meadow.Units;
+using System;
+using System.Threading;
+using AU = Meadow.Units.Angle.UnitType;
+
+namespace Grove.Servo_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var servo = new Servo(Device.CreatePwmPort(Device.Pins.D13));
+
+            servo.RotateTo(new Angle(0, AU.Degrees));
+
+            while (true)
+            {
+                for (int i = 0; i <= servo.Config.MaximumAngle.Degrees; i++)
+                {
+                    servo.RotateTo(new Angle(i, AU.Degrees));
+                    Console.WriteLine($"Rotating to {i}");
+                    Thread.Sleep(40);
+                }
+                Thread.Sleep(2000);
+                for (int i = 180; i >= servo.Config.MinimumAngle.Degrees; i--)
+                {
+                    servo.RotateTo(new Angle(i, AU.Degrees));
+                    Console.WriteLine($"Rotating to {i}");
+                    Thread.Sleep(40);
+                }
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
