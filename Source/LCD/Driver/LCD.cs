a2d1e378b62123d1f559cce3f21c35a797e1d67f
Adrian Stevens <adrian.stevens@gmail.com>
2022-03-26 20:24:04 -0700

    Add Grove LCD driver

---
+++ Source/LCD/Driver/LCD.cs
@@ -0,0 +1,29 @@
+﻿using Meadow.Foundation.Displays.Lcd;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.Grove.Displays
+{
+    /// <summary>
+    /// Represents a Grove LCD display
+    /// </summary>
+    public class LCD : CharacterDisplay
+    {
+        /// <summary>
+        /// Creates a Grove LCD display object
+        /// </summary>
+        /// <param name="i2cBus"></param>
+        /// <param name="address"></param>
+        /// <param name="rows"></param>
+        /// <param name="columns"></param>
+        public LCD(II2cBus i2cBus,
+            byte address = (byte)I2cCharacterDisplay.Addresses.Grove,
+            byte rows = 2, byte columns = 16)
+            :base(i2cBus,
+                 address,
+                 rows,
+                 columns,
+                 true)
+        {
+        }
+    }
+}
---
+++ Source/LCD/Samples/LCD_Sample/MeadowApp.cs
@@ -0,0 +1,36 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Grove.Displays;
+using System;
+using System.Threading;
+
+namespace Grove.LCD_Sample
+{
+    // Change F7MicroV2 to F7Micro for V1.x boards
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            var display = new LCD(Device.CreateI2cBus());
+
+            display.Write("Hello Grove");
+
+            Thread.Sleep(2000);
+
+            for(byte i = 0; i < 16; i++)
+            {
+                display.SetCursorPosition(i, 0);
+                display.Write("1");
+                Thread.Sleep(250);
+
+                display.SetCursorPosition(i, 1);
+                display.Write("2");
+                Thread.Sleep(250);
+            }
+
+            display.ClearLines();
+        }
+    }
+}
