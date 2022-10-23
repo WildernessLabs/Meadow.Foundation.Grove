9dbe86319b83e15049716bbea9928d7023d48422
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-10-22 21:29:00 -0700

    Add Grove.Drivers.sln and switch drivers to local library references

--- Source/Speaker/Driver/Speaker.cs
+++ Source/Speaker/Driver/Speaker.cs
@@ -1,5 +1,6 @@
 ﻿using Meadow.Foundation.Audio;
 using Meadow.Hardware;
+using Meadow.Units;
 
 namespace Meadow.Foundation.Grove.Audio
 {
@@ -11,6 +12,14 @@ namespace Meadow.Foundation.Grove.Audio
         /// <summary>
         /// Creates a Speaker driver
         /// </summary>
+        /// <param name="port"></param>
+        public Speaker(IPwmPort port)
+            : base(port)
+        { }
+
+        /// <summary>
+        /// Creates a Speaker driver
+        /// </summary>
         /// <param name="device"></param>
         /// <param name="pin"></param>
         /// <param name="frequency"></param>
@@ -18,21 +27,13 @@ namespace Meadow.Foundation.Grove.Audio
         public Speaker(
             IPwmOutputController device, 
             IPin pin, 
-            float frequency = 100, 
+            Frequency frequency, 
             float dutyCycle = 0)
             : base(
-                  device, 
-                  pin, 
-                  frequency, 
-                  dutyCycle)
-        { }
-
-        /// <summary>
-        /// Creates a Speaker driver
-        /// </summary>
-        /// <param name="port"></param>
-        public Speaker(IPwmPort port)
-            : base(port)
+                device, 
+                pin, 
+                frequency, 
+                dutyCycle)
         { }
     }
 }
