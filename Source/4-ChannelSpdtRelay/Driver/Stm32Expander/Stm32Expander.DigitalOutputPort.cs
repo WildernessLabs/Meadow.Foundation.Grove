35f1ec2090b5ba7625328c8e9612f78251e2ff5a
Adrian Stevens <adrian.stevens@gmail.com>
2023-10-23 23:18:51 -0700

    Add 4-Channel SPDT Relay

---
+++ Source/4-ChannelSpdtRelay/Driver/Stm32Expander/Stm32Expander.DigitalOutputPort.cs
@@ -0,0 +1,47 @@
+﻿using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Grove.Relays
+{
+    internal partial class Stm32Expander
+    {
+        /// <summary>
+        /// A Stm32Expander FourChannelSptdRelay-specific implementation of the IDigitalOutputPort
+        /// </summary>
+        internal partial class DigitalOutputPort : DigitalOutputPortBase
+        {
+            internal event EventHandler Disposed = delegate { };
+
+            /// <summary>
+            /// The port's containing Stm32Expander
+            /// </summary>
+            public Stm32Expander Peripheral { get; }
+
+            /// <summary>
+            /// Creates a DigitalOutputPort instance
+            /// </summary>
+            public DigitalOutputPort(Stm32Expander peripheral, IPin pin, bool initialState, OutputType initialOutputType = OutputType.PushPull)
+                : base(pin, (IDigitalChannelInfo)pin!.SupportedChannels![0], initialState, initialOutputType)
+            {
+                Peripheral = peripheral;
+
+                State = initialState;
+            }
+
+            /// <inheritdoc/>
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+                Disposed?.Invoke(this, EventArgs.Empty);
+            }
+
+            /// <inheritdoc/>
+            public override bool State
+            {
+
+                get => Peripheral.GetPinState(Pin);
+                set => Peripheral.SetPinState(Pin, value);
+            }
+        }
+    }
+}
